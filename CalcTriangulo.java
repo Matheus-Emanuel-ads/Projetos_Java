e651d02b5d066f99f86155bfd08c5a25e279eece
Matheus-Emanuel-UCB <matheusemanuel.corporativo@gmail.com>
2025-08-14 16:08:56 -0300

    Atualizado e melhorado

--- CalcTriangulo.java
+++ CalcTriangulo.java
@@ -3,4 +3,12 @@ package curso_programacao;
 public class CalcTriangulo {
 
 	public double a, b, c;
+	
+	public double area(){
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p* (p - a) * (p - b) * (p - c));
+		
+	}
+	
+
 }
